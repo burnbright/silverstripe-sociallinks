a588d4f7629ca5ba39253780062fbbbc903427fe
helpfulrobot <chris@silverstripe.com>
2016-01-01 00:25:04 +1300

    Converted to PSR-2

--- code/SocialSiteConfig.php
+++ code/SocialSiteConfig.php
@@ -1,32 +1,31 @@
 <?php
 
-class SiteConfig_SocialLinks extends DataExtension{
-	
-	static $many_many =  array(
-		'SocialLinks' => 'SocialLink'
-	);
+class SiteConfig_SocialLinks extends DataExtension
+{
+    
+    public static $many_many =  array(
+        'SocialLinks' => 'SocialLink'
+    );
 
-	static $many_many_extraFields = array(
-		'SocialLinks' => array(
-			'Sort' => 'Int'
-		)
-	);
-	
-	function updateCMSFields(FieldList $fields){
-		
-		$fields->addFieldToTab("Root.SocialLinks",
-			GridField::create(
-				"SocialLinks",
-				"Social Network Links",
-				$this->owner->SocialLinks(),
-				$config = GridFieldConfig_RelationEditor::create()
-			)
-		);
+    public static $many_many_extraFields = array(
+        'SocialLinks' => array(
+            'Sort' => 'Int'
+        )
+    );
+    
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->addFieldToTab("Root.SocialLinks",
+            GridField::create(
+                "SocialLinks",
+                "Social Network Links",
+                $this->owner->SocialLinks(),
+                $config = GridFieldConfig_RelationEditor::create()
+            )
+        );
 
-		if(class_exists("GridFieldOrderableRows")){
-			$config->addComponent(new GridFieldOrderableRows());
-		}
-		
-	}
-	
+        if (class_exists("GridFieldOrderableRows")) {
+            $config->addComponent(new GridFieldOrderableRows());
+        }
+    }
 }
